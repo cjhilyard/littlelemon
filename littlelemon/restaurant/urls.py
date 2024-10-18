1e16f562872132b5000ec6f68623671d2808bda9
cjhilyard <chuck.j.hilyard@gmail.com>
2024-10-17 20:04:29 -0700

    module3

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -2,8 +2,10 @@
 #define URL route for index() view
 from django.urls import path
 from .views import BookingView, MenuView
+from rest_framework.authtoken.views import ObtainAuthToken
 
 urlpatterns = [
     path('booking/', BookingView.as_view(), name='booking'),
     path('menu/', MenuView.as_view(), name='menu' ),
+    path('api-token-auth/', ObtainAuthToken.as_view(), name='api-token-auth'),
 ]
