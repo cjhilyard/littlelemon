d14bf9ffeb58ad7b078e49532352cc2fc5399a77
cjhilyard <chuck.j.hilyard@gmail.com>
2024-10-26 19:58:25 -0700

    module5 bookings sorta work

--- littlelemon/restaurant/forms.py
+++ littlelemon/restaurant/forms.py
@@ -3,6 +3,8 @@ from .models import Booking
 from django import forms
 
 class BookingForm(forms.ModelForm):
+    """ docstring
+    """
     class Meta:
         model = Booking
         fields = '__all__'
